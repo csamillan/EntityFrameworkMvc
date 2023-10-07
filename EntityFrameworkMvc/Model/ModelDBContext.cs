f5227cd75abd503fe5716bd8eac1280d29a56b7a
Carlos A <csamillanm@gmail.com>
2023-10-07 16:47:11 -0500

    Creations the method for list in Entity Editorials and Books.

--- EntityFrameworkMvc/Model/ModelDBContext.cs
+++ EntityFrameworkMvc/Model/ModelDBContext.cs
@@ -12,11 +12,42 @@ namespace EntityFrameworkMvc.Model
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //Creacion de Data Inicial Para Editorial
+            List<Editorial> editorialsInit = new List<Editorial>();
+
+            editorialsInit.Add(new Editorial
+            {
+                Id = 1,
+                Name = "Libreria San Marcos",
+                NameContact = "Manuel Fernandez",
+                Address = "Urb. Salamanca Mz 4 Lt 4",
+                City = "Lima",
+                Phone = "+51555555555",
+                Email ="libSanMarcos@gmail.com",
+                Commentary = "Libreria especializada en libros de programacion"
+            });
+
+            editorialsInit.Add(new Editorial
+            {
+                Id = 2,
+                Name = "Libreria Santo Tomas",
+                NameContact = "Jose Alfredo",
+                Address = "Av. Los Manzanos N° 548",
+                City = "Chiclayo",
+                Phone = "+51888888888",
+                Email = "libSantoTomas@gmail.com",
+                Commentary = "Libreria especializada en libros para niños"
+            });
+
             modelBuilder.Entity<Editorial>( Editorial =>
             {
+                //To Table permite ponerle el nombre q qieres a la tabla
                 Editorial.ToTable("Editorial");
+
+                //Has Key Permite crear la clave primaria al valor
                 Editorial.HasKey(p => p.Id);
 
+                //Permite asignar la Propiedad de cada columna de cada columna
                 Editorial.Property(p => p.Name)
                                         .IsRequired()
                                         .HasMaxLength(50);
@@ -38,6 +69,35 @@ namespace EntityFrameworkMvc.Model
                 Editorial.Property(p => p.Commentary)
                                         .IsRequired(false)
                                         .HasMaxLength(150);
+
+                //HasData permite el llenado de la data inicial en la tabla
+                Editorial.HasData(editorialsInit);
+            });
+
+            List<Book> booksInit = new List<Book>();
+
+            booksInit.Add(new Book()
+            {
+                Id = 1,
+                ISBN = "54545adsasd541215",
+                Title = "Estructura e interpretación de programas informáticos",
+                Autor = "Harold Abelson",
+                Year = 2005,
+                Price = 28,
+                Commentary = "Libro basicos para estudiantes de programacion",
+                EditorialId = 1,
+            });
+
+            booksInit.Add(new Book()
+            {
+                Id = 2,
+                ISBN = "6465123321asdasd321586",
+                Title = "A Veces Mamá Tiene Truenos en la Cabeza",
+                Autor = "Harold Abelson",
+                Year = 2021,
+                Price = 35,
+                Commentary = "Libro para niños entre 8 a 12 años",
+                EditorialId = 2,
             });
 
             modelBuilder.Entity<Book>(Book =>
@@ -61,6 +121,8 @@ namespace EntityFrameworkMvc.Model
                 Book.Property(p => p.Commentary)
                                         .IsRequired(false)
                                         .HasMaxLength(150);
+
+                Book.HasData(booksInit);
             });
         }
     }
--- EntityFrameworkMvc/Program.cs
+++ EntityFrameworkMvc/Program.cs
@@ -1,11 +1,16 @@
 using EntityFrameworkMvc.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Creacion la variable de conexion para la base de datos.
 builder.Services.AddDbContext<ModelDBContext>(options =>
 {
     var connectionStr = builder.Configuration.GetConnectionString("MvcEF") ??
@@ -19,9 +24,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//Utilizacion de la conexion y creacion de base de datos.
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetService<ModelDBContext>();
+    //context.Database.EnsureDeleted(); //Eliminar Base de datos.
     context.Database.EnsureCreated();
 }
 
@@ -35,7 +42,17 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
+
+//Creacion de metodos de listar para validar data.
+app.MapGet("/api/editorial", ([FromServices] ModelDBContext dbcontext) =>
+{
+    return Results.Ok(dbcontext.Editorials.ToList());
+});
+
+app.MapGet("/api/book", ([FromServices] ModelDBContext dbcontext) =>
+{
+    return Results.Ok(dbcontext.Books.ToList());
+});
 
 app.Run();
